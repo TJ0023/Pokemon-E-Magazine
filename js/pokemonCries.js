3519f8b6880106e9d0d3aa0a22f243e385487112
TJ0023 <GimenezTJV@gmail.com>
2022-05-30 06:21:20 +0800

    "Uploading concept web design and necessary files which are already compressed"

---
+++ js/pokemonCries.js
@@ -0,0 +1,41 @@
+
+    let x = document.getElementById("pikachuCry"); 
+    y = document.getElementById("charizardCry");
+    z = document.getElementById("garchompCry");
+    a = document.getElementById("miloticCry");
+    b = document.getElementById("reshiramCry");
+    c = document.getElementById("vanilluxeCry");
+    d = document.getElementById("tyranitarCry");
+    e = document.getElementById("steelixCry");
+    function playPikachu() { 
+        x.play(); 
+        x.volume = 0.25;
+    } 
+    function playCharizard() { 
+        y.play(); 
+        y.volume = 0.25;
+    } 
+    function playGarchomp() {
+        z.play();
+        z.volume = 0.25;
+    }
+    function playMilotic() {
+        a.play();
+        a.volume = 0.25;
+    }
+    function playReshiram() {
+        b.play();
+        b.volume = 0.25;
+    }
+    function playVanilluxe() {
+        c.play();
+        c.volume = 0.25;
+    }
+    function playTyranitar() {
+        d.play();
+        d.volume = 0.25;
+    }
+    function playSteelix() {
+        e.play();
+        e.volume = 0.25;
+    }
