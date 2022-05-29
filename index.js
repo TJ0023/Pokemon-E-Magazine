b1672491d55abb1e1f9e78ca8a4d2a2bb5b32d05
TJ0023 <GimenezTJV@gmail.com>
2022-05-30 06:17:15 +0800

    "Finalized website design hopefully!"

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 function toggle() {
     document.body.classList.toggle('dark');
 }
+
+function openSlide(){
+    document.getElementById('slide-menu').style.width = '50vw';
+    }
+
+    function closeSlide(){
+    document.getElementById('slide-menu').style.width = '0vw';
+    }
